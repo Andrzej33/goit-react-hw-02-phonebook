c8dbabe58de6532c0a207d8ee711fb75754e60d5
Andrij Vasylkiv <andrijvasilkiv7@gmail.com>
2023-04-18 12:03:40 +0300

    add name checked

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,30 +17,51 @@ import { Filter } from './PhoneBook/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    // contacts: [],
+    // contacts: [
+    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    // ],
+    contacts: [],
     filter: '',
   };
 
   // addContacts = text =>{}
 
   addContact = ({name,number}) => {
-  
-const contact ={
-  id: nanoid(),
-  name,
-  number,
-}
 
-    this.setState(({contacts}) => ({
-      
-      contacts:[...contacts,contact]
-    }))
+    if(
+      this.state.contacts.find(contact => contact.name.toLowerCase() === name.toLowerCase())
+    )
+
+    // if(this.setState((prevState) => {
+    //   prevState.contacts.find(contact => contact.name === name);
+    //   console.log(name);
+    //   // console.log(contact);
+    // }))
+    
+    {
+      alert(`${name} is already in contacts`);
+      return
+    }
+    
+
+
+
+  {
+    const contact ={
+      id: nanoid(),
+      name,
+      number,
+    }
+    
+        this.setState(({contacts}) => ({
+          
+          contacts:[...contacts,contact]
+        }))
+  }
+
   };
 
   changeFilter = e => {
